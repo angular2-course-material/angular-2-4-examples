d0e04e681510da6451b791b84b35309ffb13524a
Daniela Meneses <daniela11290@gmail.com>
2017-03-10 02:47:08 -0400

    Child routes and router outlets

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,15 +4,30 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { ProfileComponent } from './profile/profile.component';
+import { EditComponent } from './edit/edit.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: '', component: ProfileComponent }
+      { 
+        path: '',
+        component: WrapperComponent,
+        children: [
+          {
+            path: 'profile',
+            component: ProfileComponent
+          },
+          {
+            path: ':id/edit',
+            component: EditComponent
+          }
+        ]
+      }
     ])
   ],
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, EditComponent, WrapperComponent],
   exports: [RouterModule]
 })
 export class UserModule { }
