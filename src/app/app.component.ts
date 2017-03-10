0a1e534a4861126104d60126c5f052bb455d077a
Daniela Meneses <daniela11290@gmail.com>
2017-03-10 03:32:05 -0400

    Basic Observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app works!';
+  observable;
+  subscription;
+  values = [];
+
+  constructor() {
+    this.observable = new Observable(observer => {
+      setTimeout(() => {
+          observer.next(42);
+      }, 1000);
+
+      setTimeout(() => {
+          observer.next(43);
+      }, 2000);
+
+      setTimeout(() => {
+          observer.next(44);
+      }, 4000);
+
+      setTimeout(() => {
+          observer.complete();
+      }, 6000);
+    });
+  }
+
+  ngOnInit() {
+    this.subscription = this.observable.subscribe(
+      value => this.values.push(value),
+      error => console.log('Error: ', error),
+      () => console.log('FINISHED')
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
